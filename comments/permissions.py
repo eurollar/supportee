596e3bc9b4daa6d834f2bb03b8289e629b02b1df
eurollar <fakekrolchatka@gmail.com>
2022-05-12 23:59:19 +0300

    Add permissions for get/create comments only for ticket's author or support

--- comments/permissions.py
+++ comments/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
 
+from tickets.models import Ticket
+
 
 class IsSupportOrAuthor(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.type == 'support')
-
-    def has_object_permission(self, request, view, obj):
-        return bool(obj.author == request.user or request.user.type == 'support')
+        queryset = Ticket.objects.get(pk=view.kwargs['pk'])
+        ticket_author = queryset.author.username
+        return bool(request.user.type == 'support' or request.user.username == ticket_author)
